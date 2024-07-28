068570823870f4406e9518b032279e004b8fe367
hgaogao <hgaogaogao@gmail.com>
2024-07-28 15:11:23 +0800

    update: wezterm

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,15 +1,15 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Azu (Gogh)'
+config.color_scheme = "Azu (Gogh)"
 
--- 关闭时不进行确认
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-config.window_close_confirmation = 'NeverPrompt'
-config.font = wezterm.font('JetBrains Mono')
+-- 关闭时不进行确认
+config.window_close_confirmation = "NeverPrompt"
+config.font = wezterm.font("JetBrains Mono")
 
 -- powershell 7
 config.default_prog = { "c:\\Program Files\\PowerShell\\7\\pwsh.exe", "-NoLogo" }
